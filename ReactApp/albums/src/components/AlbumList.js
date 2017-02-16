9df3b944f7e3d8a0eb57f10fa6464a1526fedea2
cjwoodru2 <cjwoodru2@gmail.com>
2017-02-15 20:54:09 -0500

    adding cards to display album content from API

--- ReactApp/albums/src/components/AlbumList.js
+++ ReactApp/albums/src/components/AlbumList.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List!</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  state = { albums: [] };
+  componentWillMount() {
+    return fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((response) => response.json())
+      .then((responseData) => this.setState({ albums: responseData }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+  render() {
+    console.log(this.state.albums);
+    return (
+      <View>
+        {this.renderAlbums()}
+
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
