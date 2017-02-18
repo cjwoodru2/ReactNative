901ba7d3b9b81a210aa363f9c3bd5574d1187f04
cjwoodru2 <cjwoodru2@gmail.com>
2017-02-18 10:39:37 -0500

    reset the auth project because of errors

--- ReactApp/auth/src/components/common/index.js
+++
@@ -1,9 +0,0 @@
-export * from './Button';
-export * from './Header';
-export * from './Card';
-export * from './CardSection';
-export * from './Input';
-export * from './Spinner'
-
-// Folder is to make it easier to import multiple reusable components
-// These will be common components that are used throughout the app
